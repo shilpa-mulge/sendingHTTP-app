ed471ffb096cf8fc6b50a224d3bd0781a329f236
Shilpa <mulgeshilpa@gmail.com>
2023-02-18 20:16:26 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,28 +6,60 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function FetchMovies() {
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-    const translatedData = data.results.map(movie => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        releaseDate: movie.release_date,
-        openingText: movie.opening_crawl,
+  const FetchMovies = useCallback(async () => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/film/');
+      if (!response.ok) {
+        throw new Error("some thing went wrong....Retrying")
       }
-    })
-    setMovies(translatedData)
-  }
+      const data = await response.json();
 
+      const translatedData = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        }
+      })
+      setMovies(translatedData)
+    }
+    catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }, [])
+
+  useEffect(() => {
+    const intid = setInterval(() => {
+      if (error)
+        FetchMovies();
+    }, 5000
+    )
+    return () => clearInterval(intid)
+  }, [error])
+
+  function cancelButtonHandler() {
+    setError(null)
+    setIsLoading(false)
+
+  }
   return (
     <React.Fragment>
       <section>
         <button onClick={FetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found</p>}
+        {isLoading && <p>Loading....</p>}
+        {error && <div><p>{error}</p>
+          <button onClick={cancelButtonHandler}>cancel</button></div>}
       </section>
     </React.Fragment>
   );
